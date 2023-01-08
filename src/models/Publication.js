34993c8ba697fc60b4fec8b561b8220dda978237
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-08 13:12:55 +0200

    model for publications

---
+++ src/models/Publication.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const publicationSchema = new mongoose.Schema({
+    title: {
+        type: String, 
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    _ownerId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Publication = mongoose.model('Publication', publicationSchema);
+
+module.exports = Publication;
