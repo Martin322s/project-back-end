9845b0d1cf8c3b1b4d5b1d42db7ba41d450baf7b
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-08 13:15:28 +0200

    route for publications

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authController = require('./controllers/authController');
+const publicationController = require('./controllers/publicationController');
 
 router.use('/users', authController);
+router.use('/publications', publicationController);
 
 module.exports = router;
